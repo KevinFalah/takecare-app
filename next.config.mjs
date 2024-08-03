ad136d9a46b64875c0e550d760f01670856148fc
KevinFalah <kevinghossanfalah34@gmail.com>
2024-08-03 11:46:33 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
     };
     return config;
   },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
